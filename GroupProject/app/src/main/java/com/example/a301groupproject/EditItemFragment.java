10100b292cc93fe7a02dd51efdd4ac8d3656aa48
YangYang_G <146893848+yangyang-guo03@users.noreply.github.com>
2023-11-05 22:39:03 -0700

    Update EditItemFragment.java

--- GroupProject/app/src/main/java/com/example/a301groupproject/EditItemFragment.java
+++ GroupProject/app/src/main/java/com/example/a301groupproject/EditItemFragment.java
@@ -1,12 +1,10 @@
 package com.example.a301groupproject;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -18,13 +16,18 @@ import com.example.a301groupproject.databinding.FragmentAddItemBinding;
 import com.example.a301groupproject.factory.item.Item;
 import com.example.a301groupproject.ui.home.HomeViewModel;
 
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class EditItemFragment extends Fragment {
 
     private FragmentAddItemBinding binding;
 
     private HomeViewModel homeViewModel;
+
 
     public EditItemFragment() {
     }
@@ -39,15 +42,19 @@ public class EditItemFragment extends Fragment {
 
         binding.confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
+            // not done with the limits
             public void onClick(View v) {
                 String itemName = binding.itemNameInput.getText().toString();
                 String itemModel = binding.itemModelInput.getText().toString();
                 String itemMake = binding.itemMakeInput.getText().toString();
-                String itemDate = binding.itemDateInput.getText().toString();
                 String estimatedValue = binding.estimatedValueInput.getText().toString();
                 String serialNumber = binding.serialNumberInput.getText().toString();
                 String description = binding.descriptionInput.getText().toString();
                 String comment = binding.commentInput.getText().toString();
+                String year = binding.inputYear.getText().toString();
+                String month = binding.inputMonth.getText().toString();
+                String day = binding.inputDay.getText().toString();
+                String itemDate = year + "-" + month + '-' + day;
 
 
                 //notification of all required properties
@@ -66,8 +73,18 @@ public class EditItemFragment extends Fragment {
                     return;
                 }
 
-                if (itemDate.isEmpty()) {
-                    Toast.makeText(getContext(), "Please enter the item date", Toast.LENGTH_SHORT).show();
+                if (year.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter yy", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (month.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter mm", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (day.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter dd", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -86,9 +103,18 @@ public class EditItemFragment extends Fragment {
                     return;
                 }
 
+                try {
+                    SimpleDateFormat dateInForm = new SimpleDateFormat("yyyy-MM-dd");
+                    Date dateFormatted = dateInForm.parse(itemDate);
+                } catch (ParseException e) {
+                    Toast.makeText(getContext(), "Please enter date in yyyy-mm-dd format", Toast.LENGTH_SHORT).show();
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getContext(), "Please enter a valid number", Toast.LENGTH_SHORT).show();
+                }
 
                 Item item = new Item(itemName, itemModel, itemMake, itemDate, estimatedValue, serialNumber, description,comment);
                 homeViewModel.addItem(item);
+
 
                 // go back to home page after add confirm
                 NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
