fb56484a96464f42df99bfa346f9b74262901b2f
liaoguest09 <122191079+liaoguest09@users.noreply.github.com>
2023-11-07 18:13:49 -0700

    updated

--- GroupProject/app/src/main/java/com/example/a301groupproject/EditItemFragment.java
+++ GroupProject/app/src/main/java/com/example/a301groupproject/EditItemFragment.java
@@ -1,8 +1,6 @@
 package com.example.a301groupproject;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +13,11 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.example.a301groupproject.databinding.FragmentAddItemBinding;
-import com.example.a301groupproject.factory.item.Item;
+import com.example.a301groupproject.factory.Item;
 import com.example.a301groupproject.ui.home.HomeViewModel;
+import com.example.a301groupproject.ui.home.HomeFragment;
 
-import java.util.ArrayList;
+import java.util.Locale;
 
 public class EditItemFragment extends Fragment {
 
@@ -37,78 +36,28 @@ public class EditItemFragment extends Fragment {
         View view = binding.getRoot();
 
         homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
-        Bundle receivedBundle = getArguments();
 
-        if (receivedBundle != null) {
-            int receivedIntValue = (receivedBundle.getInt("loc"));
-
-            Item i = homeViewModel.getItems().getValue().get(receivedIntValue);
-
-            binding.itemNameInput.setText(i.getName());
-            binding.itemModelInput.setText(i.getModel());
-            binding.itemMakeInput.setText(i.getMake());
-            binding.itemDateInput.setText(i.getDate());
-            binding.estimatedValueInput.setText(i.getValue());
-
-            homeViewModel.emptyImages();
-            ArrayList<String> imageUris = i.getImages();
-            for(String uri :imageUris){
-                homeViewModel.addImage(Uri.parse(uri));
-
-            }
-
-            binding.deleteButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    homeViewModel.removeItem(homeViewModel.getItems().getValue().get(receivedIntValue));
-                    NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
-                    navController.navigateUp();
-
-                }
-            });
-        }
         binding.confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // TODO: Need to contain all the attributes (comment, desc...)
                 String itemName = binding.itemNameInput.getText().toString();
                 String itemModel = binding.itemModelInput.getText().toString();
                 String itemMake = binding.itemMakeInput.getText().toString();
                 String itemDate = binding.itemDateInput.getText().toString();
                 String estimatedValue = binding.estimatedValueInput.getText().toString();
 
-                ArrayList<String> imageUris = new ArrayList<>();
-                ArrayList<Uri> value = homeViewModel.getImages().getValue();
-
-                for (Uri uri : value) {
-                    imageUris.add(uri.toString());
-                }
-
                 Item item = new Item(itemName, itemModel, itemMake, itemDate, estimatedValue);
-                if(receivedBundle == null) {
-                    homeViewModel.addItem(item, imageUris);
-                    homeViewModel.emptyImages();
-                }
-                else {
-                    int receivedIntValue = (receivedBundle.getInt("loc"));
-                    Item i = homeViewModel.getItems().getValue().get(receivedIntValue);
-                    item.setId(i.getId());
-                    homeViewModel.editItem(item,imageUris);
-                }
+                homeViewModel.addItem(item);
 
-                // go back to home page after add confirm
                 NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
                 navController.navigateUp();
             }
         });
 
         binding.imageButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
-                navController.navigate(R.id.nav_images);
+
             }
         });
         return view;
--- GroupProject/app/src/main/java/com/example/a301groupproject/ui/home/HomeViewModel.java
+++ GroupProject/app/src/main/java/com/example/a301groupproject/ui/home/HomeViewModel.java
@@ -1,110 +1,49 @@
 package com.example.a301groupproject.ui.home;
 
-import android.net.Uri;
-
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.a301groupproject.factory.item.Item;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.example.a301groupproject.factory.Item;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class HomeViewModel extends ViewModel {
     private final MutableLiveData<ArrayList<Item>> items = new MutableLiveData<>(new ArrayList<>());
 
-    private MutableLiveData<ArrayList<Uri>> images = new MutableLiveData<>(new ArrayList<>());
-
-    private FirebaseFirestore db;
-    private FirebaseUser user;
-
-    public MutableLiveData<ArrayList<Uri>> getImages() {
-        return images;
-    }
-
-    public void addImage(Uri uri) {
-        ArrayList<Uri> imagesValue = images.getValue();
-        imagesValue.add(uri);
-        images.setValue(imagesValue);
-    }
-
-    public void emptyImages() {
-        images.setValue(new ArrayList<>());
-    }
-
     public MutableLiveData<ArrayList<Item>> getItems() {
-        db = FirebaseFirestore.getInstance();
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String uid = user.getUid();
-            db.collection("users").document(uid).collection("items")
-                    .addSnapshotListener((snapshots, e) -> {
-                        ArrayList<Item> itemList = new ArrayList<>();
-                        if (snapshots != null) {
-                            for (DocumentSnapshot doc : snapshots) {
-                                Item item = doc.toObject(Item.class);
-                                item.setId(doc.getId());
-                                itemList.add(item);
-                            }
-                            items.setValue(itemList);
-                        }
-                    });
-        }
-
         return items;
     }
 
-    // add to database
-    public void addItem(Item item, ArrayList<String> imageUris) {
-        db = FirebaseFirestore.getInstance();
-        user = FirebaseAuth.getInstance().getCurrentUser();
-
-        Map<String, Object> itemData = new HashMap<>();
-
-        itemData.put("name", item.getName());
-        itemData.put("model", item.getModel());
-        itemData.put("make", item.getMake());
-        itemData.put("date", item.getDate());
-        itemData.put("value", item.getValue());
-        itemData.put("images", imageUris);
-
-        if (user != null) {
-            String uid = user.getUid();
-
-            // TODO: implement successful and fail message
-            db.collection("users").document(uid).collection("items").document().set(itemData);
+    public void addItem(Item item) {
+        ArrayList<Item> itemsValue = items.getValue();
+        if (item != null) {
+            itemsValue.add(item);
+            items.setValue(itemsValue);
         }
     }
 
-    // TODO: need to be rewritten
     public void removeItem(Item item) {
-//        ArrayList<Item> itemsValue = items.getValue();
-//        if (item != null) {
-//            itemsValue.remove(item);
-//            items.setValue(itemsValue);
-//        }
-        db = FirebaseFirestore.getInstance();
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        db.collection("users").document(user.getUid()).collection("items").document(item.getId()).delete();
+        ArrayList<Item> itemsValue = items.getValue();
+        if (item != null) {
+            itemsValue.remove(item);
+            items.setValue(itemsValue);
+        }
     }
 
-    public void editItem(Item item,ArrayList<String> imageUris) {
-        db = FirebaseFirestore.getInstance();
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        DocumentReference itemRef = db.collection("users").document(user.getUid()).collection("items").document(item.getId());
-        Map<String, Object> updatedData = new HashMap<>();
-        updatedData.put("name", item.getName());
-        updatedData.put("model", item.getModel());
-        updatedData.put("make", item.getMake());
-        updatedData.put("date", item.getDate());
-        updatedData.put("value", item.getValue());
-        updatedData.put("images", imageUris);
-        itemRef.set(updatedData);
+    public double calculateTotalValue() {
+        double total = 0.0;
+        ArrayList<Item> itemsList = items.getValue();
+        if (itemsList != null) {
+            for (Item item : itemsList) {
+                try {
+                    total += Double.parseDouble(item.getValue());
+                } catch (NumberFormatException e) {
+                    // 在实际项目中你可能需要更合适的错误处理
+                    e.printStackTrace();
+                }
+            }
+        }
+        return total;
     }
+
 }
