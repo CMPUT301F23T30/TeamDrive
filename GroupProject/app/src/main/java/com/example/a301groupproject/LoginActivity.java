c6bf233ff99e557f9c589316bc19406aaedb6bf9
StevenX27 <fx4@ualberta.ca>
2023-11-09 01:58:35 -0700

    Login activity ui test done

--- GroupProject/app/src/main/java/com/example/a301groupproject/LoginActivity.java
+++ GroupProject/app/src/main/java/com/example/a301groupproject/LoginActivity.java
@@ -11,6 +11,14 @@ import com.example.a301groupproject.ui.login.LoginFragment;
 import com.google.firebase.FirebaseApp;
 
 public class LoginActivity extends AppCompatActivity {
+    /**
+     * Called when the activity is starting. Login Activity is where most initialization should go:
+     * calling setContentView(int) to inflate the activity's UI, using findViewById(int)
+     * to programmatically interact with widgets in the UI
+     * This acts as a host like a container for the actual login fragment and signup fragments
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
